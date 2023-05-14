e79b81d142b68a8e81f95cdb34fdff3bfc16d8be
Mofeid Moshreky <mof_ton@yahoo.com>
2023-05-14 07:18:37 -0400

    app.js

--- app.js
+++ app.js
@@ -18,3 +18,47 @@ function closeFullDP() {
     x.style.display = "flex";
   }
 }
+
+function openFullScreenDP() {
+  var x = document.getElementById("fullScreenDP");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
+
+function isEnter(event) {
+  if (event.keyCode == 13) {
+    sendMsg();
+  }
+}
+
+function sendMsg() {
+  var input = document.getElementById("inputMSG");
+  var ti = input.value;
+  if (input.value == "") {
+    return;
+  }
+  var date = new Date();
+  var myLI = document.createElement("li");
+  var myDiv = document.createElement("div");
+  var greendiv = document.createElement("div");
+  var dateLabel = document.createElement("label");
+  dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+  myDiv.setAttribute("class", "sent");
+  greendiv.setAttribute("class", "green");
+  dateLabel.setAttribute("class", "dateLabel");
+  greendiv.innerText = input.value;
+  myDiv.appendChild(greendiv);
+  myLI.appendChild(myDiv);
+  greendiv.appendChild(dateLabel);
+  document.getElementById("listUL").appendChild(myLI);
+  var s = document.getElementById("chatting");
+  s.scrollTop = s.scrollHeight;
+  setTimeout(function () {
+    waitAndResponce(ti);
+  }, 1500);
+  input.value = "";
+  playSound();
+}
